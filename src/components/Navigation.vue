2bcc577ea0e7c12a4f550fc3a86aa6917998f756
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2021-04-20 14:34:45 +0200

    Navigation Component
    Mj and Robin

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,59 @@
 <template>
-  
+  <div class="nav-bar">
+    <div class="nav-menu">
+      <button class="menu-button" @click="goToPage('/')">Hem</button>
+
+      <button class="menu-button" @click="goToPage('/rules')">Regler</button>
+
+      <button class="menu-button" @click="goToPage('/history')">Historia</button>
+
+      <button class="menu-button" @click="goToPage('/highscore')">Topp 10</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    goToPage(page) {
+      const currentPage = this.$route.path;
+      if (currentPage !== page) {
+        this.$router.push(page);
+      }
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.menu-button {
+  width: 90px;
+  height: 40px;
+  background-color: #b6d4c6;
+  border-radius: 8px;
+  place-items: center;
+  margin: 1rem;
+  cursor: pointer;
+  color: #464545;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14pt;
+  border: 0;
+  outline: 0;
+  transition-duration: 0.4s;
+}
 
+.menu-button:hover {
+  background-color: #EE9292; 
+  color: #464545;
+  box-shadow: 1px 8px 8px 0 rgba(0,0,0,0.2);
+}
+
+.nav-bar {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 4.2rem;
+  background-color: #407378;
+  margin: 0;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h1>This is the start page</h1>
+    <Navigation />
+    <h1>This is Home</h1>
   </div>
 </template>
+
+<script>
+import Navigation from '../components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation
+  }
+
+  
+}
+</script>
+
+<style>
+
+h1{
+  margin: 0;
+}
+
+</style>
