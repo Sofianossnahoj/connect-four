73b91d85b7c19cd1d6daa4d1f27595562734d8bb
Vic Graf <victoria.b.graf@gmail.com>
2021-04-21 15:28:48 +0200

    Rules-spelling

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -1,6 +1,15 @@
 <template>
-    <main>
-    <h1>Regler</h1>
+  <main> 
+    <section>
+      <h1>Regler</h1>
+      <p>Filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler filler</p>
+    </section>
+    <section>
+      <img src="../assets/horizontal-win.jpg" alt="horizontal-win" class="horizontal-win" >
+      <img src="../assets/vertical-win.jpg" alt="vertical-win" class="vertical-win">
+      <img src="../assets/diagonal-45-win.jpg" alt="diagonal-45-win" class="diagonal-45-win">
+      <img src="../assets/diagonal-135-win.jpg" alt="diagonal-135-win" class="diagonal-135-win">
+    </section>
   </main>
 </template>
 
@@ -14,5 +23,16 @@ export default {
 main {
   width: 100vw;
   height: 100vh;
+}
+
+/* .rules-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+} */
+
+h1 {
+  font-family: 'Rajdhani', sans-serif;
+  font-size: 70px;
 }
 </style>
