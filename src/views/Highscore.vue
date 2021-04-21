7d3435e13c656870e0fd56a3e6fefe0b0790c082
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2021-04-21 16:53:12 +0200

    hamburger
    added the icon, list of places to go, still needs v-if

--- src/views/Highscore.vue
+++ src/views/Highscore.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>This is the highscore page</h1>
+  <div class="highscore">
+    <Navigation />
+    <h1>TOPP 10 page</h1>
   </div>
 </template>
+
+<script>
+
+import Navigation from '../components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
