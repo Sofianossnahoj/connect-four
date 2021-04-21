fd0b3b283ec51a7edc4b49bb439c3b68ad97f865
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2021-04-21 09:41:55 +0200

    home page mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,61 @@
 <template>
-  <div>
-    <Navigation />
-    <h1>This is Home</h1>
-  </div>
+
+  <main class="homeLanding">
+    <section class="homeLogo">
+      <h1>Välkommen till fyra i rad</h1> 
+    </section>
+    <router-link class="start__btn" to="/landing">
+      start
+    </router-link>
+  </main>
+  
 </template>
 
 <script>
-import Navigation from '../components/Navigation.vue'
-
 export default {
-  components: {
-    Navigation
-  }
 
-  
 }
 </script>
 
 <style>
+.homeLanding {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
+.homeLogo{
+  background: url("../assets/LogoMarkers.svg") bottom no-repeat ;
+  display: flex;
+  justify-content: center;
+  height: 30%;
+  margin-bottom: 40px;
+  margin-left: 10px;
+  margin-right:10px;
+}
 h1{
-  margin: 0;
+  display: flex;
+  width: 100%;
+  font-family: 'Ribeye', cursive;
+  color: #464545;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 36px;
+text-shadow: 0px 1px 0px rgb(129, 128, 128);
+}
+.start__btn{
+height: 50px;
+width: 234px;
+background-color: #424040;
+border-radius: 4px;
+color: #C4C4C4;
+font-family: 'Rajdhani', sans-serif;
+font-size: 36px;
+text-align: center;
+text-decoration: none;
 }
 
 </style>
