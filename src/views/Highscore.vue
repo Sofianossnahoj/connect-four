093af5723b10980e79306ea8a3297441753d4101
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2021-04-22 16:07:52 +0200

    Update Highscore.vue
    not done but work in progress

--- src/views/Highscore.vue
+++ src/views/Highscore.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="highscore">
+  <main>
     <Navigation />
-    <h1>TOPP 10 page</h1>
-  </div>
+    <div class="highscore">
+      <div class="highscore-title">
+          <h2>TOPP 10 in progress</h2>
+      </div>
+      <div class="top-list">
+        <p class="yellow">1. Mom</p>
+        <p class="green">2. Dad</p>
+        <p class="yellow">3. Sam</p>
+        <p class="green">4. Sara</p>
+        <p class="yellow">5. Batman</p>
+        <p class="green">6. Wolverine</p>
+        <p class="yellow">7. Storm</p>
+        <p class="green">8. Theo</p>
+        <p class="yellow">9. Tom</p>
+        <p class="green">10. Lily</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -16,6 +32,58 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.highscore {
+  width: 800px;
+  margin-left: 20%; 
+}
+
+.highscore-title {
+  font-family: 'Rajdhani', sans-serif;
+  font-size: 2.7rem;
+  color: #464545;
+  margin-top: 1.5rem;
+}
+
+.top-list {
+  font-family: 'Rajdhani', sans-serif;
+  font-size: 2rem;
+  max-width: 800px;
+  margin: 0;
+}
+
+p {
+  margin: .5rem;
+  color: #464545;
+}
+
+p.yellow {
+  background-color: #DECF98;
+}
+
+@media screen and (max-width: 800px) {
+  h2{
+    font-size: 36px;
+    margin-left: 1rem;
+  }
+
+  .top-list {
+    max-width: 350px;
+  }
+
+  .highscore {
+    margin-left: 5%;
+    width: 100%;
+  }
+}
+
+
 
 </style>
