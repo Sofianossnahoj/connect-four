640129635e98788efb1e847af83ab49a81d9a2b1
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2021-04-23 16:43:07 +0200

    Gamestart/landing inital commit
    + rework on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,45 @@
 <template>
-
   <main>
-    <desktopNavigation v-if="!desktopView"/>
-    <article class="home__landing">
-      <section class="home__logo">
-        <h3>Välkommen till </h3>
-        <h1>fyra i rad</h1> 
-    </section>
-      <router-link class="start__btn" to="/landing">
+    <Navigation v-if="!desktopView" />
+    <article class="home-landing">
+      <section class="home-logo">
+        <h3>Välkommen till</h3>
+        <h1>fyra i rad</h1>
+      </section>
+      <router-link class="start-btn" to="/landing">
         start
       </router-link>
     </article>
   </main>
-  
 </template>
 
 <script>
-import DesktopNavigation from "../components/Navigation.vue"; 
+import Navigation from "../components/Navigation.vue";
 export default {
-  
-
- data: () => {
-   return {
-     desktopView: true,
-     showNavbar: true
-   }
-   },
-   components: {
-    DesktopNavigation
+  data: () => {
+    return {
+      desktopView: true,
+    };
+  },
+  components: {
+    Navigation,
   },
 
   methods: {
-    handleView(){
-      this.desktopView = window.innerWidth <=500;
-    }
+    handleView() {
+      this.desktopView = window.innerWidth <= 800;
+    },
   },
 
   created() {
     this.handleView();
-    window.addEventListener('resize', this.handleView);
-  }
- }
+    window.addEventListener("resize", this.handleView);
+  },
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Ribeye&family=Open+Sans&family=Rajdhani:wght@300&display=swap');
-
-.home__landing {
+.home-landing {
   width: 100vw;
   min-height: 100vh;
   display: flex;
@@ -56,69 +49,66 @@ export default {
   overflow: scroll;
 }
 
-.home__logo{
-  background: url("../assets/LogoMarkers.svg") bottom no-repeat ;
+.home-logo {
+  background: url("../assets/LogoMarkers.svg") bottom no-repeat;
   height: 25%;
   width: 100%;
   margin-bottom: 20px;
   margin-left: 10px;
-  margin-right:10px;
+  margin-right: 10px;
 }
 h3 {
   display: flex;
   height: 25%;
   width: 100%;
   font-size: 36px;
-  font-family: 'Rajdhani', sans-serif;
+  font-family: "Rajdhani", sans-serif;
   color: #464545;
   justify-content: center;
 }
-h1{
+h1 {
   display: flex;
   width: 100%;
   height: 25%;
   justify-content: center;
-  font-family: 'Ribeye', cursive;
+  font-family: "Ribeye", cursive;
   color: #464545;
   text-align: center;
   text-transform: uppercase;
   font-size: 36px;
   margin-top: -40px;
   padding-bottom: 20px;
-
 }
-.start__btn{
-height: 50px;
-width: 234px;
-background-color: #424040;
-border-radius: 4px;
-color: #C4C4C4;
-font-family: 'Rajdhani', sans-serif;
-font-size: 36px;
-text-align: center;
-text-decoration: none;
+.start-btn {
+  display: flex;
+  height: 50px;
+  width: 234px;
+  background-color: #424040;
+  border-radius: 4px;
+  color: #c4c4c4;
+  font-family: "Rajdhani", sans-serif;
+  font-size: 36px;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
 }
 
-@media screen and (min-width: 500px){
-  
-
-
- .start__btn{
-   width: 292px;
-   height: 60px;
- }
+@media screen and (min-width: 500px) {
+  .start-btn {
+    width: 292px;
+    height: 60px;
+  }
 }
-@media screen and (min-width: 620px){
-  
- .home__logo {
-   background-size: 545px 107px;
-   height: 50%;
- }
- h1 {
-   font-size: 96px;
- }
- h3{
-   font-size: 72px;
- }
+@media screen and (min-width: 620px) {
+  .home-logo {
+    background-size: 545px 107px;
+    height: 50%;
+  }
+  h1 {
+    font-size: 96px;
+  }
+  h3 {
+    font-size: 72px;
+  }
 }
 </style>
